23e347e378838155a529af623b7cd0bb131b99ac
jafflores <jorgeantoniofloreshdez@hotmail.com>
2025-11-04 15:51:52 -0700

    add posgre Routes and Controller

---
+++ src/db.js
@@ -0,0 +1,27 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+export const query = (text, params) => {
+    return pool.query(text, params);
+};
+
+const initializaDataBase = async () => {
+    try {
+        await query(`
+            CREATE TABLE IF NOT EXISTS posts (
+                id SERIAL PRIMARY KEY,
+                content TXT NOT NULL,
+                crated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+        console.log('Database initialized');
+    }
+    catch (error) {
+        console.error('Error initializing database:', error);
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import postRoutes from './routes/postRoutes.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use('api/posts', postRoutes);
+
 app.listen(port);
 
 app.get('/', (req, res) => {
     res.send('Hello World!!');
 });
 console.log(`Server is running on http://localhost:${port}`);
-
-
