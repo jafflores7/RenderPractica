090f4571ac4b7921f66a137b79715d9a17f049db
jafflores <jorgeantoniofloreshdez@hotmail.com>
2025-11-11 15:46:55 -0700

    fixed typo en postController.js

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,7 +6,7 @@ export const createPost = async (req, res) => {
         const insertPostQuery = `
             INSERT INTO posts (content) 
             VALUES ($1) 
-            RETURNING id, content, crated_at; 
+            RETURNING id, content, created_at; 
         `;
         const result = await query(insertPostQuery, [content]);
         res.json(result.rows[0]);
