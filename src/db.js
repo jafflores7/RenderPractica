c1667fa26c312b9962cdf782800c347cb746533c
jafflores <jorgeantoniofloreshdez@hotmail.com>
2025-11-11 15:33:25 -0700

    fix db

--- src/db.js
+++ src/db.js
@@ -2,11 +2,15 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export const query = (text, params) => {
-    return pool.query(text, params);
+    return pool.query(text, params)
+    
 };
 
 const initializeDataBase = async () => {
